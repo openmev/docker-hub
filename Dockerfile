8aa24d5842581a8b01f65bf61b9998fbef8cd35f
sam bacha <sam@manifoldfinance.com>
2021-10-01 17:56:02 -0700

    build(docker): configure dockerhub
    Signed-off-by: sam bacha <sam@manifoldfinance.com>

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+# syntax=docker/dockerfile:1
 FROM golang:1.17-alpine AS builder
 
 RUN apk add --no-cache gcc musl-dev linux-headers git
--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ git clone https://github.com/flashbots/mev-geth --branch v1.10.8-mev0.4.0 --sing
 cd go-ethereum/
 echo "building $VERSION"
 echo "Docker BuildX Starting..."
-
-DOCKER_BUILDKIT=1 docker buildx build . -t openmev/mev-geth-alpine:v1.10.8-mev0.4.0
+DOCKER_BUILDKIT=1 docker buildx build go-ethereum/ -t openmev/mev-geth-alpine:v1.10.8-mev0.4.0
+#DOCKER_BUILDKIT=1 docker buildx build . -t openmev/mev-geth-alpine:v1.10.8-mev0.4.0
