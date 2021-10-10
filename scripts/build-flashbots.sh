d9eb70c849d7146e439262c3c51d809fe5a1e31e
sam bacha <sam@manifoldfinance.com>
2021-10-10 08:03:36 -0700

    versioning

--- scripts/build-flashbots.sh
+++ scripts/build-flashbots.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 VERSION=v1.10.8-mev0.4.0
+CONTAINER_VERSION=$(jq -r ".version" ../../package.json)
 
+echo "Building OpenMEV-${CONTAINER_VERSION}-${VERSION}"
 git clone https://github.com/flashbots/mev-geth --branch v1.10.8-mev0.4.0 --single-branch go-ethereum
 cd go-ethereum/
 echo "building $VERSION"
 echo "Docker BuildX Starting..."
-DOCKER_BUILDKIT=1 docker buildx build go-ethereum/ -t openmev/mev-geth-alpine:v1.10.8-mev0.4.0
+
 #DOCKER_BUILDKIT=1 docker buildx build . -t openmev/mev-geth-alpine:v1.10.8-mev0.4.0
+DOCKER_BUILDKIT=1 docker buildx build go-ethereum/ -t openmev/mev-geth-alpine:$VERSION
